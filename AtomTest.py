68e2a3d2182e599fcae92d34059886159150d478
Eugene <e.karchevsky@mail.ru>
2020-10-11 18:57:42 +0400

    Final version

--- AtomTest.py
+++ AtomTest.py
@@ -1,2 +1,2 @@
 print ("This mini-programm will count the number of characters in your text")
-print ("Your text has " + str(len(input())) + " simbols"))
+print ("Your text has " + str(len(input())) + " simbols")
